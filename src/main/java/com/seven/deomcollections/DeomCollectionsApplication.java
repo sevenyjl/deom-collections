2d7de0f6c6f04e969a14630b54d2d0f7f51b7dfa
1399435579@qq.com <1399435579@qq.com>
2022-06-20 22:29:11 +0800

    init project

---
+++ src/main/java/com/seven/deomcollections/DeomCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.seven.deomcollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeomCollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeomCollectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seven/deomcollections/DeomCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seven.deomcollections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeomCollectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
